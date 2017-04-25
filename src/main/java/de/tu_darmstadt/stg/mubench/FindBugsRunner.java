f224ce965d1cd1bac5260c07d0912da7a5c5afe6
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2017-04-25 17:10:18 +0200

    mubench detector interface for findbugs

---
+++ src/main/java/de/tu_darmstadt/stg/mubench/FindBugsRunner.java
@@ -0,0 +1,95 @@
+package de.tu_darmstadt.stg.mubench;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.tu_darmstadt.stg.mubench.cli.CodePath;
+import de.tu_darmstadt.stg.mubench.cli.DetectorArgs;
+import de.tu_darmstadt.stg.mubench.cli.DetectorFinding;
+import de.tu_darmstadt.stg.mubench.cli.DetectorOutput;
+import de.tu_darmstadt.stg.mubench.cli.MuBenchRunner;
+import edu.umd.cs.findbugs.BugCollectionBugReporter;
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.DetectorFactoryCollection;
+import edu.umd.cs.findbugs.FindBugs2;
+import edu.umd.cs.findbugs.MethodAnnotation;
+import edu.umd.cs.findbugs.Priorities;
+import edu.umd.cs.findbugs.Project;
+import edu.umd.cs.findbugs.config.UserPreferences;
+
+public class FindBugsRunner extends MuBenchRunner {
+
+	public static void main(String[] args) throws Exception {
+		new FindBugsRunner().run(args);
+	}
+
+	@Override
+	protected void detectOnly(DetectorArgs args, DetectorOutput output) throws Exception {
+		runFindBugs(args, output);
+	}
+
+	@Override
+	protected void mineAndDetect(DetectorArgs args, DetectorOutput output) throws Exception {
+		runFindBugs(args, output);
+	}
+
+	private void runFindBugs(DetectorArgs args, DetectorOutput output)
+			throws FileNotFoundException, IOException, InterruptedException {
+		FindBugs2 findbugs = new FindBugs2();
+		Project targetProject = buildProject(args.getTargetPath());
+		BugReporter bugReporter = new BugCollectionBugReporter(targetProject);
+		bugReporter.setPriorityThreshold(Priorities.LOW_PRIORITY);
+		findbugs.setUserPreferences(UserPreferences.createDefaultUserPreferences());
+		findbugs.setProject(targetProject);
+		findbugs.setBugReporter(bugReporter);
+		findbugs.setDetectorFactoryCollection(DetectorFactoryCollection.instance());
+		findbugs.execute();
+		for (BugInstance bug : findbugs.getBugReporter().getBugCollection()) {
+			MethodAnnotation primaryMethod = bug.getPrimaryMethod();
+			DetectorFinding finding = output.add(primaryMethod.getSourceFileName(), primaryMethod.getMethodName());
+			finding.put("rank", String.valueOf(bug.getBugRank()));
+			finding.put("desc", bug.getMessage());
+			finding.put("type", bug.getType());
+			finding.put("startline", String.valueOf(bug.getPrimarySourceLineAnnotation().getStartLine()));
+			finding.put("endline", String.valueOf(bug.getPrimarySourceLineAnnotation().getEndLine()));
+		}
+	}
+
+	private List<File> findSourceFiles(String dir) {
+		return getFiles(dir, ".java");
+	}
+
+	private List<File> findClassFiles(String dir) {
+		return getFiles(dir, ".class");
+	}
+
+	private Project buildProject(CodePath path) {
+		Project project = new Project();
+		List<File> files = findClassFiles(path.classPath);
+		files.addAll(findSourceFiles(path.srcPath));
+		for (File file : files) {
+			project.addFile(file.getAbsolutePath());
+		}
+		return project;
+	}
+
+	private List<File> getFiles(String dir, String suffix) {
+		File directory = new File(dir);
+		List<File> files = new ArrayList<>();
+		// get all the files from a directory
+		File[] fList = directory.listFiles();
+		for (File file : fList) {
+			if (file.isFile() && file.getName().contains(suffix)) {
+				files.add(file);
+			} else if (file.isDirectory()) {
+				files.addAll(getFiles(file.getAbsolutePath(), suffix));
+			}
+		}
+		return files;
+	}
+
+}
